a51d4a548ff518091a9b5181e162e3735de11b80
Patrick Kelly <p-kell@live.com>
2020-08-13 09:06:25 -0400

    Migrated Rune implementation using most modern .NET Runtime implementation details.

---
+++ Rune/Text/RuneExtensions.cs
@@ -0,0 +1,34 @@
+﻿#if NETSTANDARD1_3
+using System.Text;
+
+namespace System {
+	/// <summary>
+	/// Implements various <see cref="Rune"/> related methods that aren't directly part of <see cref="Rune"/>.
+	/// </summary>
+	public static class RuneExtensions {
+		/// <summary>
+		/// Returns an enumeration of <see cref="Rune"/> from this string.
+		/// </summary>
+		/// <remarks>
+		/// Invalid sequences will be represented in the enumeration by <see cref="Rune.ReplacementChar"/>.
+		/// </remarks>
+		public static StringRuneEnumerator EnumerateRunes(this String @string) => new StringRuneEnumerator(@string);
+
+		/// <summary>
+		/// Returns an enumeration of <see cref="Rune"/> from the provided span.
+		/// </summary>
+		/// <remarks>
+		/// Invalid sequences will be represented in the enumeration by <see cref="Rune.ReplacementChar"/>.
+		/// </remarks>
+		public static SpanRuneEnumerator EnumerateRunes(this ReadOnlySpan<Char> span) => new SpanRuneEnumerator(span);
+
+		/// <summary>
+		/// Returns an enumeration of <see cref="Rune"/> from the provided span.
+		/// </summary>
+		/// <remarks>
+		/// Invalid sequences will be represented in the enumeration by <see cref="Rune.ReplacementChar"/>.
+		/// </remarks>
+		public static SpanRuneEnumerator EnumerateRunes(this Span<Char> span) => new SpanRuneEnumerator(span);
+	}
+}
+#endif
